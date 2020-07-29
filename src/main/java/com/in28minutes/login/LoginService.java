1f1e8e77e32f6881f51a046e2ca81f2911b4553b
Smit <amitsahube@gmail.com>
2020-07-29 18:34:47 +0530

    commit msg

--- src/main/java/com/in28minutes/login/LoginService.java
+++ src/main/java/com/in28minutes/login/LoginService.java
@@ -5,6 +5,15 @@ import org.springframework.stereotype.Service;
 public class Challenge{
 	
 	public static void main()
+	{
+		int var = 5;
+		//showing demo
 		
+		
+	}
+	
+	public void conflictTest() {
+		int var = 2;
+	}
 	
 }
